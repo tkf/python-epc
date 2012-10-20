117b7c7162e654a12094b72daae7dc7ccedbb63a
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-20 04:24:01 +0200

    Add more links in README

--- epc/__init__.py
+++ epc/__init__.py
@@ -3,10 +3,20 @@
 EPC (RPC stack for Emacs Lisp) server for Python
 ================================================
 
-
 Links:
 
+.. Host documentation at Read the Docs (but not now).
+   * `Documentaions (at Read the Docs) <http://python-epc.readthedocs.org/>`_
+
+* `Repository (at GitHub) <https://github.com/tkf/python-epc>`_
+* `Issue tracker (at GitHub) <https://github.com/tkf/python-epc/issues>`_
+* `PyPI <http://pypi.python.org/pypi/python-epc>`_
+
+.. Run test on Travis CI at some point (but not now).
+   * `Travis CI <https://travis-ci.org/#!/tkf/python-epc>`_
+
 * `kiwanami/emacs-epc <https://github.com/kiwanami/emacs-epc>`_
+  (Client and server implementation in Emacs Lisp and Perl.)
 
 """
 # [[[end]]]
