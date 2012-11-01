02b92e22a1eebcdf6625afa76c7c8f6dd2fd5184
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-01 21:07:02 +0100

    Add Python 3 etc. to setup.py classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,12 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         "Programming Language :: Emacs-Lisp",
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
