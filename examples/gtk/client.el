c900f4149a8ee3fc5ee6c72b831d43d954492474
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-11 02:34:27 +0100

    Add docstrings in gtk/client.el

--- examples/gtk/client.el
+++ examples/gtk/client.el
@@ -9,16 +9,20 @@
 (defvar pyepc-sample-gtk-dir
   (if load-file-name
       (file-name-directory load-file-name)
-    default-directory))
+    default-directory)
+  "Path to examples/gtk/.")
 
 (defvar pyepc-sample-gtk-epc
   (epc:start-epc (or (getenv "PYTHON") "python")
-                 (list (expand-file-name "server.py" pyepc-sample-gtk-dir))))
+                 (list (expand-file-name "server.py" pyepc-sample-gtk-dir)))
+  "EPC manager object for GTK server example.")
 
 (defun pyepc-sample-gtk-destroy ()
+  "Close GTK window"
   (interactive)
   (epc:call-deferred pyepc-sample-gtk-epc 'destroy nil))
 
 (defun pyepc-sample-gtk-set-button-label (label)
+  "Change GUI button label."
   (interactive "sButton label: ")
   (epc:call-deferred pyepc-sample-gtk-epc 'set_button_label (list label)))
