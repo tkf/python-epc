6b274642bd95d05e3f9454ae16abbd58494add33
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-12 19:19:21 +0100

    Add epc.core.itermessage (gut of EPCHandler._recv)

---
+++ epc/core.py
@@ -0,0 +1,11 @@
+def itermessage(read):
+    while True:
+        head = read(6)
+        if not head:
+            return
+        length = int(head, 16)
+        data = read(length)
+        if len(data) < length:
+            raise ValueError('need {0}-length data; got {1}'
+                             .format(length, len(data)))
+        yield data
