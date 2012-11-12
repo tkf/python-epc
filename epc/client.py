fc227df91b996a0d469c78c4d36cae019d678505
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-12 20:37:35 +0100

    Simple EPCClient implementation

--- epc/client.py
+++ epc/client.py
@@ -1,3 +1,8 @@
+import itertools
+
+from .core import itermessage, encode_message, decode_message
+
+
 class SocketReader(object):
 
     def __init__(self, sock, recvsize=4096):
@@ -11,3 +16,42 @@ class SocketReader(object):
         value = self._buffer[:size]
         self._buffer = self._buffer[size:]
         return value
+
+
+class EPCClient(object):
+
+    def __init__(self, socket_or_address):
+        if isinstance(socket_or_address, tuple):
+            import socket
+            self.socket = socket(socket_or_address)
+        else:
+            self.socket = socket
+        self.reader = SocketReader(self.socket)
+        self._messages = itermessage(self.reader.read)
+        counter = itertools.count(1)
+        self.get_uid = lambda: next(counter)
+
+    def register_function(self, function, name=None):
+        raise NotImplementedError
+
+    def call(self, method, *args):
+        return self._request('call', method, *args)
+
+    def methods(self):
+        return self._request('methods')
+
+    def _request(self, name, *args):
+        uid = self.get_uid()
+        self.socket.sendall(encode_message(name, uid, *args))
+        (name, ruid, rest) = decode_message(next(self._messages))
+        assert uid == ruid  # FIXME: support non-serial execution!
+        return getattr(self, '_handle_{0}'.format(name))(uid, *rest)
+
+    def _handle_return(self, uid, reply):
+        return reply
+
+    def _handle_return_error(self, uid, reply):
+        raise ValueError(reply)
+
+    def _handle_epc_error(self, uid, reply):
+        raise ValueError(reply)
