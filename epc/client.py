c749e1a8bb1ce56c35f877c6642bb0ff428f50d8
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-14 20:40:04 +0100

    Use EPCHandler for client
    * In StreamRequestHandler, self.request is actually a socket object,
      so that I can use it in client side.
    
    * I don't need SocketReader anymore.  Actually, using socket.makefile
      as in StreamRequestHandler.setup was the right way to do it.

--- epc/client.py
+++ epc/client.py
@@ -1,27 +1,7 @@
+import threading
 import itertools
 
-from sexpdata import Symbol
-
-from .core import itermessage, encode_message, unpack_message
-from .utils import ThreadedIterator
-
-
-class SocketReader(object):
-
-    def __init__(self, sock, recvsize=4096):
-        self._sock = sock
-        self._recvsize = recvsize
-        self._buffer = bytearray()
-
-    def read(self, size):
-        while len(self._buffer) < size:
-            got = self._sock.recv(self._recvsize)
-            if not got:
-                return ''
-            self._buffer.extend(got)
-        value = self._buffer[:size]
-        self._buffer = self._buffer[size:]
-        return bytes(value)
+from .server import EPCHandler
 
 
 class EPCClient(object):
@@ -38,31 +18,18 @@ class EPCClient(object):
             self.socket = socket.create_connection(socket_or_address)
         else:
             self.socket = socket_or_address
-        self.reader = SocketReader(self.socket)
-        self._messages = ThreadedIterator(itermessage(self.reader.read))
 
-    def register_function(self, function, name=None):
-        raise NotImplementedError
+        # This is what BaseServer.finish_request does:
+        address = None  # it is not used, so leave it empty
+        self.handler = EPCHandler(self.socket, address, self)
 
-    def call(self, method, args):
-        return self._request('call', Symbol(method), args)
+        self.call = self.handler.call
+        self.methods = self.handler.methods
 
-    def methods(self):
-        return self._request('methods')
+        self.handler_thread = threading.Thread(target=self.handler.handle)
+        self.handler_thread.start()
 
-    def _request(self, name, *args):
-        uid = self.get_uid()
-        self.socket.sendall(encode_message(name, uid, *args))
-        (name, ruid, rest) = unpack_message(next(self._messages))
-        pyname = name.replace('-', '_')
-        assert uid == ruid  # FIXME: support non-serial execution!
-        return getattr(self, '_handle_{0}'.format(pyname))(uid, *rest)
-
-    def _handle_return(self, uid, reply):
-        return reply
-
-    def _handle_return_error(self, uid, reply):
-        raise ValueError(reply)
-
-    def _handle_epc_error(self, uid, reply):
-        raise ValueError(reply)
+    def _ignore(*_):
+        """"Do nothing method for `EPCHandler`."""
+    add_client = _ignore
+    remove_client = _ignore
