2927efb35255dd1e0cf63ce3e405c4221eca8d41
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-15 22:54:03 +0100

    Add test_server_calls_client_echo/bad_method
    They fail occasionally...

--- epc/tests/test_py2py.py
+++ epc/tests/test_py2py.py
@@ -2,6 +2,7 @@ from ..client import EPCClient
 from ..server import ThreadingEPCServer
 from ..server import ReturnError
 from ..utils import newthread
+from ..py3compat import Queue
 from .utils import BaseTestCase
 
 
@@ -13,6 +14,9 @@ class TestEPCPy2Py(BaseTestCase):
         self.server.daemon_threads = True
         self.server_thread = newthread(self, target=self.server.serve_forever)
         self.server_thread.start()
+
+        self.client_queue = q = Queue.Queue()
+        self.server.handle_client_connect = q.put
 
         self.client = EPCClient(self.server.server_address)
 
@@ -33,9 +37,21 @@ class TestEPCPy2Py(BaseTestCase):
         self.server.shutdown()
         self.server.server_close()
 
+    def wait_until_client_is_connected(self):
+        self.client_queue.get(timeout=1)
+
     def test_client_calls_server_echo(self):
         self.assertEqual(self.client.call_sync('echo', [55]), [55])
 
     def test_client_calls_server_bad_method(self):
         self.assertRaises(
             ReturnError, self.client.call_sync, 'bad_method', [55])
+
+    def test_server_calls_client_echo(self):
+        self.wait_until_client_is_connected()
+        self.assertEqual(self.server.clients[0].call_sync('echo', [55]), [55])
+
+    def test_server_calls_client_bad_method(self):
+        self.wait_until_client_is_connected()
+        self.assertRaises(
+            ReturnError, self.server.clients[0].call_sync, 'bad_method', [55])
