e3d14cdb78e5d9f997f018e55ec616be027c1e07
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-16 11:38:35 +0100

    Add instruction to use yappi

--- examples/inprocess/echo.py
+++ examples/inprocess/echo.py
@@ -10,6 +10,21 @@ To see the profiling resut, use::
 
     python -m pstats
 
+However, what you can see in the profile is the profile of
+the main thread.
+
+To see the profile of all threads, use yappi_.
+To use yappi, run the following snippet::
+
+    import yappi
+    yappi.start()
+    echo_long_string(int(1e5), 1)
+    yappi.stop()
+    yappi.print_stats(limit=10, sort_type=yappi.SORTTYPE_TSUB)
+    yappi.print_stats(limit=10, sort_type=yappi.SORTTYPE_TTOT)
+
+.. _yappi: http://code.google.com/p/yappi/
+
 """
 
 
