4010a86e77c08a2949b922f78c41579db2f9de0d
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-16 13:09:12 +0100

    Use deferred when calling client pong from examples/epcs/server.el
    examples/epcs/run.sh hangs again

--- examples/epcs/server.el
+++ examples/epcs/server.el
@@ -8,7 +8,14 @@
        (epc:define-method mngr 'echo 'identity)
        (epc:define-method
         mngr 'ping-pong
-        (lambda (&rest args) (epc:call-sync mngr 'pong args)))))
+        (lambda (&rest args)
+          (message "EPCS> PING-PONG got: %S" args)
+          (deferred:$
+            (epc:call-deferred mngr 'pong args)
+            (deferred:nextc it
+              (lambda (&rest args)
+                (message "EPCS> PONG returns: %S" args)
+                args)))))))
    9999))
 
 (when noninteractive
