e153cc841512980028f4557c4c8e4741bd291a29
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-17 22:20:52 +0100

    Minor change on epc.uitls.newthread

--- epc/utils.py
+++ epc/utils.py
@@ -39,14 +39,14 @@ def autolog(level):
     return wrapper
 
 
-def newname(template="EPCThread-{0}"):
+def newname(template):
     global _counter
     _counter = _counter + 1
     return template.format(_counter)
 _counter = 0
 
 
-def newthread(template, **kwds):
+def newthread(template="EPCThread-{0}", **kwds):
     """
     Instantiate :class:`threading.Thread` with an appropriate name.
     """
