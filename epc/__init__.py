bcbd6ab7dfafb06fb8bbcbd6abcbccfbec9e88ae
Takafumi Arakaki <aka.tkf@gmail.com>
2012-11-20 03:36:27 +0100

    Bump to 0.0.2.dev1

--- epc/__init__.py
+++ epc/__init__.py
@@ -130,6 +130,6 @@ see `examples/gtk/server.py`_.  You can run this example by::
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.0.2.dev0'
+__version__ = '0.0.2.dev1'
 __author__ = 'Takafumi Arakaki'
 __license__ = 'BSD License'
