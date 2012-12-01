6687a2789780b65e520ab7850d8b809a232f5908
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-01 23:41:56 +0100

    Bump to 0.0.2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ copyright = u'2012, Takafumi Arakaki'
 # The short X.Y version.
 version = '0.0.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.2.dev1'
+release = '0.0.2'
 
 exclude_patterns = []
 
