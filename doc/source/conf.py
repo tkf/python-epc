d95c3974910b338f7384b76d38f1d7e1a6cb9f1a
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-01 23:49:32 +0100

    Bump to 0.0.3.dev0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,9 +21,9 @@ project = u'Python EPC'
 copyright = u'2012, Takafumi Arakaki'
 
 # The short X.Y version.
-version = '0.0.2'
+version = '0.0.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.0.2'
+release = '0.0.3.dev0'
 
 exclude_patterns = []
 
